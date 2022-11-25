464620f7ce65a70efd1d5d625626d1f770345821
HafizGhulamAbas <hgabbbas1122@gmail.com>
2022-11-25 16:11:30 +0500

    Accessibility Testing: Setup Cypress-axe into project

---
+++ cypress/e2e/accessibility/todo-app.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+
+describe('Todo application', () => {
+    it('should log any accessibility failures', () => {
+        cy.visit('http://todomvc.com/examples/react')
+        cy.injectAxe()
+        cy.checkA11y()
+    })
+  })
