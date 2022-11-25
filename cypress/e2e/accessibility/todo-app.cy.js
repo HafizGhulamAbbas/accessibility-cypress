f4d9334eb9bde83ed7d2c64bdedf542c5e797ea1
HafizGhulamAbas <hgabbbas1122@gmail.com>
2022-11-25 16:52:40 +0500

    Accessibility Testing: Exclude specific rule

--- cypress/e2e/accessibility/todo-app.cy.js
+++ cypress/e2e/accessibility/todo-app.cy.js
@@ -23,4 +23,10 @@ describe('Todo application', () => {
     it('should only include rules with serious and critical impacts', () => {
         cy.checkA11y(null, { includedImpacts: ['critical', 'serious'] })
     })
+
+    it('should exclude specific accessibility rules', () => {
+        cy.checkA11y(null, { rules: {
+            'color-contrast' : { enabled: false }
+        } })
+    })
   })
